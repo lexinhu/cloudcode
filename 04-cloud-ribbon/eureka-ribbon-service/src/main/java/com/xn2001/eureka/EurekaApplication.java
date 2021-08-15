b00f7cce24566029287bbbf97089faee834fe1b1
ZXH <jialna@qq.com>
2021-08-16 00:04:42 +0800

    04 ribbon

---
+++ 04-cloud-ribbon/eureka-ribbon-service/src/main/java/com/xn2001/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package com.xn2001.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
---
+++ 04-cloud-ribbon/order-service/src/main/java/com/xn2001/order/OrderApplication.java
@@ -0,0 +1,34 @@
+package com.xn2001.order;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("com.xn2001.order.mapper")
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    public IRule iRule(){
+        //默认为轮询规则，这里自定义为随机规则
+        return new RandomRule();
+    }
+
+}
