03983d0f8c353ec5f9835595a57ffcc42b6bc075
ZXH <jialna@qq.com>
2021-08-20 22:51:49 +0800

    06-cloud-feign

---
+++ 06-cloud-feign/feign-api/src/main/java/com/xn2001/feign/clients/UserClient.java
@@ -0,0 +1,12 @@
+package com.xn2001.order.feign;
+
+import com.xn2001.order.pojo.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("userservice")
+public interface UserClient {
+    @GetMapping("/user/{id}")
+    User findById(@PathVariable("id") Long id);
+}
