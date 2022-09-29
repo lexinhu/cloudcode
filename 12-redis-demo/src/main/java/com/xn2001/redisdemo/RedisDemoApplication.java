45af1333b9ffd2ed10478be04262d0bfb7d579d3
xn2001 <jialna@qq.com>
2022-09-30 04:13:47 +0800

    新增reids

---
+++ 12-redis-demo/src/main/java/com/xn2001/redisdemo/RedisDemoApplication.java
@@ -0,0 +1,21 @@
+package com.xn2001.redisdemo;
+
+import io.lettuce.core.ReadFrom;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class RedisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisDemoApplication.class, args);
+    }
+
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer clientConfigurationBuilderCustomizer(){
+        return clientConfigurationBuilder -> clientConfigurationBuilder.readFrom(ReadFrom.REPLICA_PREFERRED);
+    }
+
+}
